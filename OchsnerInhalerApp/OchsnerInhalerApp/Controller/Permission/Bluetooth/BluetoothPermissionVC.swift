68de2dd0922a24c8789195a0d2ca3b571379c054
Nikita Bhatt <nikita.bhatt@volansys.com>
2022-02-03 18:00:27 +0530

    changes revert for bluetoothpermitionVC code

--- OchsnerInhalerApp/OchsnerInhalerApp/Controller/Permission/Bluetooth/BluetoothPermissionVC.swift
+++ OchsnerInhalerApp/OchsnerInhalerApp/Controller/Permission/Bluetooth/BluetoothPermissionVC.swift
@@ -34,8 +34,7 @@ class BluetoothPermissionVC: BaseVC {
     
     //MARK: Actions
     @IBAction func tapGrant(_ sender: UIButton) {
-        BLEHelper.shared.setDelegate()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+
            BLEHelper.shared.isAllowed { isAllow in
             if isAllow {
                 UserDefaultManager.isGrantBLE = true
@@ -51,7 +50,6 @@ class BluetoothPermissionVC: BaseVC {
             }
             
           }
-        })
     }
     
     @IBAction func tapCancel(_ sender: UIButton) {
