36870eed06347200913eb4a0fa61a60ab8e5913f
Nikita Bhatt <nikita.bhatt@volansys.com>
2022-02-18 11:27:23 +0530

    database add device details

---
+++ OchsnerInhalerApp/OchsnerInhalerApp/Database/Device+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Device+CoreDataProperties.swift
+//  
+//
+//  Created by Nikita Bhatt on 18/02/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Device {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Device> {
+        return NSFetchRequest<Device>(entityName: "Device")
+    }
+
+    @NSManaged public var mac: String?
+    @NSManaged public var udid: String?
+
+}
