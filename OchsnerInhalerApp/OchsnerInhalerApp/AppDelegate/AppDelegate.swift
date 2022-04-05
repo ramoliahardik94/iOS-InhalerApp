a3bd0258130d1c74901f1ccee9bf849ebf8a9a40
Nikita Bhatt <nikita.bhatt@volansys.com>
2022-04-05 09:24:12 +0530

    Foreground time get Logs

--- OchsnerInhalerApp/OchsnerInhalerApp/AppDelegate/AppDelegate.swift
+++ OchsnerInhalerApp/OchsnerInhalerApp/AppDelegate/AppDelegate.swift
@@ -113,16 +113,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     @objc func foregroundCall() {
         print("App moved to foreground")
-//        if UserDefaultManager.isLogin  && UserDefaultManager.isGrantBLE && UserDefaultManager.isGrantLaocation && UserDefaultManager.isGrantNotification {
-//            if BLEHelper.shared.discoveredPeripheral != nil {
-//                switch BLEHelper.shared.discoveredPeripheral!.state {
-//                case .disconnected:
-//                    BLEHelper.shared.connectPeriPheral()
-//                default:
-//                    break
-//                }
-//            }
-//        }
+        if UserDefaultManager.isLogin  && UserDefaultManager.isGrantBLE && UserDefaultManager.isGrantLaocation && UserDefaultManager.isGrantNotification {
+            if BLEHelper.shared.discoveredPeripheral != nil {
+                switch BLEHelper.shared.discoveredPeripheral!.state {
+                case .disconnected:
+                    BLEHelper.shared.connectPeriPheral()
+                case .connected:
+                    BLEHelper.shared.getAccuationNumber()
+                default:
+                    break
+                }
+            }
+        }
     }
     
     @objc func backgroundCall() {
