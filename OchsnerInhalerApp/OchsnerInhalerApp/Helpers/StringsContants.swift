fd9968f684adea98d1d3a196f4e904bf29a7afdd
Nikita Bhatt <nikita.bhatt@volansys.com>
2022-04-22 10:51:21 +0530

    msg updated

--- OchsnerInhalerApp/OchsnerInhalerApp/Helpers/StringsContants.swift
+++ OchsnerInhalerApp/OchsnerInhalerApp/Helpers/StringsContants.swift
@@ -201,7 +201,7 @@ struct StringAddDevice {
     static let scanningTakeTime = "\n\n This might take a few seconds. You will be redirected to the next screen once it is complete."
     static let paringTakeTime = "\n\n This might take some time to complete. Do not press Back or Cancel."
     static let pairScreenStringArray = ["Tap", " 3 times ", "on the device within", "\n5 seconds ", "then click \"Pair Device\"."]
-    static let skipFlowAdd = "Do you want to skip the Flow of add device?"
+    static let skipFlowAdd = " Do you want to Skip or Continue the pairing process?"
     static let skipbtn = "Skip"
     static let continuebtn = "Continue"
     static let noDevice = "Devices not added yet"
